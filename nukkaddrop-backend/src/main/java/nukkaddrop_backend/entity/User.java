c742b72d3e35182d6049688a4de223aa87020aba
Gavit_Aditya <gavitaditya4@gmail.com>
2026-09-02 23:39:10 +0530

    Jwt

--- nukkaddrop-backend/src/main/java/nukkaddrop_backend/entity/User.java
+++ nukkaddrop-backend/src/main/java/nukkaddrop_backend/entity/User.java
@@ -1,6 +1,6 @@
 package nukkaddrop_backend.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 @Entity
@@ -15,7 +15,7 @@ public class User {
     private String name;
     private String email;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Enumerated(EnumType.STRING)
--- nukkaddrop-backend/src/main/java/nukkaddrop_backend/service/AuthService.java
+++ nukkaddrop-backend/src/main/java/nukkaddrop_backend/service/AuthService.java
@@ -2,6 +2,7 @@ package nukkaddrop_backend.service;
 
 import nukkaddrop_backend.entity.User;
 import nukkaddrop_backend.repository.UserRepository;
+import nukkaddrop_backend.security.JwtService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,25 +11,42 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-    public AuthService(UserRepository userRepository,PasswordEncoder passwordEncoder){
-        this.userRepository=userRepository;
-        this.passwordEncoder=passwordEncoder;
+    public AuthService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            JwtService jwtService) {
+
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
-    public User authenticate(String email,String password){
+    public User authenticate(String email, String password) {
 
         User user = userRepository.findByEmail(email)
                 .orElse(null);
 
-        if (user==null){
+        if (user == null) {
             return null;
         }
 
-        if (!passwordEncoder.matches(password, user.getPassword())){
+        if (!passwordEncoder.matches(
+                password,
+                user.getPassword())) {
+
             return null;
         }
 
         return user;
+    }
+
+    public String generateToken(User user) {
+
+        return jwtService.generateToken(
+                user.getEmail(),
+                user.getRole().name()
+        );
     }
 }
